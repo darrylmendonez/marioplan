1f031186e83821c617417dbfc896b48fa0bdc60c
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2018-12-13 19:22:27 -0500

    added cloud function auth trigger

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ const createNotification = (notification => {
   return admin.firestore().collection('notifications')
     .add(notification)
     .then(doc => console.log('notification added', doc));
-})
+});
 
 exports.projectCreated = functions.firestore
   .document('projects/{projectId}')
@@ -25,4 +25,22 @@ exports.projectCreated = functions.firestore
 
     return createNotification(notification);
 
+})
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+
+      })
 })
